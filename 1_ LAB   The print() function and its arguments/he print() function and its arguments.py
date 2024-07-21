5c29f05afa17b137f41fddeef0322fac54b4bd1d
Daniel Williams <williamsoligodaniel@gmail.com>
2024-07-21 21:43:13 +0100

    Create he print() function and its arguments.py

---
+++ 1_ LAB   The print() function and its arguments/he print() function and its arguments.py
@@ -0,0 +1,11 @@
+#Scenario
+#Modify the first line of code in the editor, using the sep and end keywords, to match the expected output. Use the two print() functions in the editor.
+
+#Don't change anything in the second print() invocation.
+
+#Expected output
+#Programming***Essentials***in...Python
+print("Programming",end="***")
+print("Essentials",end="***")
+print("in ",end="...")
+print("Python",end="")
