689edb0fdf6951d3c8693028cd9d9e8af22522ed
Daniel Williams <williamsoligodaniel@gmail.com>
2024-08-04 22:37:02 +0100

    Create String_operators.py

---
+++ variables/String_operators.py
@@ -0,0 +1,11 @@
+fname = input("May I have your first name: ")
+lname = input("May I have your last name: ")
+print("Thank you")
+print("\nYour fullname is " + fname + " " + lname)
+
+# output
+# May I have your first name: Daniel
+# May I have your last name: Williams
+# Thank you
+
+# Your fullname is Daniel Williams
