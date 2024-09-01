a73e15f0ac2e668e62777071af481e83f13d9f31
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-01 10:17:15 +0100

    Create slice.py

---
+++ lists/slice.py
@@ -0,0 +1,29 @@
+# [1:3] simply means [start:end(excluding the last index of the end)]
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[3:]
+print(new_list) # [8, 6]
+
+# this copies the entire list
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[:]
+print(new_list) # [10, 8, 6, 4, 2]
+
+# starting from index 1 = 8 and minus the last index
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:-1]
+print(new_list) # [8, 6, 4]
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[-1:1]
+print(new_list) # []
+
+# If you omit the start in your slice, it is assumed that you want to get a slice beginning at the element with index 0.
+# my_list[0:end]
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[:3]
+print(new_list) # [10, 8, 6]
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[3:]
+print(new_list) # [4, 2]
